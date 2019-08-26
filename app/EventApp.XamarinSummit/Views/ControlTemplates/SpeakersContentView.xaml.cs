9d5d338038853efea72654352cb5f0646a8b93d6
William S Rodriguez <williamsrz@outlook.com>
2019-08-26 17:36:51 -0300

    layout...

---
+++ app/EventApp.XamarinSummit/Views/ControlTemplates/SpeakersContentView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EventApp.XamarinSummit.Views.ControlTemplates
+{
+    public partial class SpeakersContentView : ContentView
+    {
+        public SpeakersContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
